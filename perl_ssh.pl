c06e7a5e7c4f3c965dac3322cff8c491168ab6d5
donetian-petkov <donetian+petkov@gmail.com>
2022-10-26 15:18:33 +0300

    SSH Custom Command

--- perl_ssh.pl
+++ perl_ssh.pl
@@ -5,49 +5,64 @@ use strict;
 
 use Net::OpenSSH;
 
-my $username = '';
-my $host = '';
-my $port = 0;
-my $private_key = '';
-my $passphrase = '';
+my %ssh_configuration;
 
-if ($#ARGV == 4 ) {
-    $username = $ARGV[0];
-    $host = $ARGV[1];
-    $port = $ARGV[2];
-    $private_key = $ARGV[3];
-    $passphrase = $ARGV[4];
+$ssh_configuration{'username'} = '';
+$ssh_configuration{'host'} = '';
+$ssh_configuration{'port'} = 0;
+$ssh_configuration{'key'} = '';
+$ssh_configuration{'passphrase'} = '';
+
+if ($#ARGV == 4) {
+    $ssh_configuration{'username'} = $ARGV[0];
+    $ssh_configuration{'host'} = $ARGV[1];
+    $ssh_configuration{'port'} = $ARGV[2];
+    $ssh_configuration{'key'} = $ARGV[3];
+    $ssh_configuration{'passphrase'} = $ARGV[4];
 }
 else {
-    print "Please provide username: ";
-    $username = <STDIN>;
-    chomp $username;
+    print "Please provide SSH username: ";
+    $ssh_configuration{'username'} = <STDIN>;
+    chomp $ssh_configuration{'username'};
 
     print "Please provide server: ";
-    $host = <STDIN>;
-    chomp $host;
+    $ssh_configuration{'host'} = <STDIN>;
+    chomp $ssh_configuration{'host'};
 
     print "Please provide port: ";
-    $port = <STDIN>;
-    chomp $port;
+    $ssh_configuration{'port'} = <STDIN>;
+    chomp $ssh_configuration{'port'};
 
     print "Please provide path to key: ";
-    $private_key = <STDIN>;
-    chomp $private_key;
+    $ssh_configuration{'key'} = <STDIN>;
+    chomp $ssh_configuration{'key'};
 
     print "Please provide private key's passphrase: ";
-    $passphrase = <STDIN>;
-    chomp $passphrase;
+    $ssh_configuration{'passphrase'} = <STDIN>;
+    chomp $ssh_configuration{'passphrase'};
 }
- 
-my $ssh = Net::OpenSSH->new($host, user => $username, port => $port, key_path => $private_key, passphrase => $passphrase);
-$ssh->error and
-  die "Couldn't establish SSH connection: ". $ssh->error;
- 
-$ssh->system("ls ~/") or
-  die "remote command failed: " . $ssh->error;
- 
-my @ls = $ssh->capture("ls");
-$ssh->error and
-  die "remote ls command failed: " . $ssh->error;
 
+print "Provide the command, which should be executed on the remote machine: ";
+my $cmd = <STDIN>;
+chomp $cmd;
+
+print "Connecting via SSH... \n";
+
+my $ssh = Net::OpenSSH->new(
+                      $ssh_configuration{'host'},
+    user           => $ssh_configuration{'username'},
+    port           => $ssh_configuration{'port'},
+    key_path       => $ssh_configuration{'key'},
+    passphrase     => $ssh_configuration{'passphrase'});
+$ssh->error and
+    die "Couldn't establish SSH connection: " . $ssh->error;
+
+my $ssh_output = $ssh->capture($cmd);
+$ssh->error and
+    die "Couldn't execute the SSH command: " . $ssh->error;
+
+open(OUTFILE, ">./ssh_output.txt") || die "Can't open ssh_output.txt for writing!\n";
+print OUTFILE $ssh_output;
+close(OUTFILE);
+
+undef $ssh;
