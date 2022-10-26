4f361ede980de0549ca148a2d834a51cb0d38c86
donetian-petkov <donetian+petkov@gmail.com>
2022-10-26 14:39:31 +0300

    Disk Usage

--- perl_disk_usage.pl
+++ perl_disk_usage.pl
@@ -1,82 +1,118 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
- 
+
 use Filesys::DiskUsage qw(du);
 use Number::Format;
 use Email::Sender::Simple qw(sendmail);
 use Email::Sender::Transport::SMTP ();
 use Email::Simple ();
-use Email::Simple::Creator (); 
+use Email::Simple::Creator ();
+use Try::Tiny;
 
 if (not @ARGV) {
     die "Usage: $0 DIRs\n";
 }
- 
-my $message = '';
-my $x = new Number::Format;
 
-my %sizes = du({'make-hash' => 1}, @ARGV);
-foreach my $entry (sort { $sizes{$a} <=> $sizes{$b} } keys %sizes) {
-    my $size = $x -> format_bytes($sizes{$entry}, unit => 'M'); 
-    print "$entry => $size\n";
-    $message = $message . "\n" . $entry . " => " . $size ;
+my $directory = $ARGV[0];
+
+print $directory . "\n";
+
+my $message = '';
+my $x = Number::Format -> new();
+
+print "Calculating disk usage.... \n";
+
+my %sizes = du({ 'make-hash' => 1 }, @ARGV);
+foreach my $entry (sort {$sizes{$a} <=> $sizes{$b}} keys %sizes) {
+    my $size = $x->format_bytes($sizes{$entry}, unit => 'M');
+    $message = $message . "\n" . $entry . " => " . $size;
 }
 
-my $smtp_server = '';
-my $smtp_port = 0;
-my $smtp_username   = '';
-my $smtp_password = '';
+print "Do you want the disk usage calculations to be printed: yes / no \n";
 
-my $should_mail = 0;
+my $print_calc = <STDIN>;
+chomp $print_calc;
+
+if (lc($print_calc) eq "yes") {
+    print $message . "\n";
+}
 
 print "Should the disk usage be sent to your email address: yes / no \n";
-if (<STDIN> eq "yes") {
-    $should_mail = 1;
+my $should_mail = <STDIN>;
+chomp $should_mail;
+
+if (lc($should_mail) eq "yes") {
+
+    my %smtp_configuration;
+
+    $smtp_configuration{'server'} = '';
+    $smtp_configuration{'port'} = 0;
+    $smtp_configuration{'username'} = '';
+    $smtp_configuration{'password'} = '';
+    my $recipient = '';
 
     print "Provide valid SMTP configuration: \n";
 
     print "SMTP Server: ";
-    $smtp_server = <STDIN>;
-    chomp $smtp_server;
+    $smtp_configuration{'server'} = <STDIN>;
+    chomp $smtp_configuration{'server'};
 
     print "SMTP Port: ";
-    $smtp_port = <STDIN>;
-    chomp $smtp_port;
+    $smtp_configuration{'port'} = <STDIN>;
+    chomp $smtp_configuration{'port'};
 
     print "SMTP Username: ";
-    $smtp_username = <STDIN>;
-    chomp $smtp_username;
+    $smtp_configuration{'username'} = <STDIN>;
+    chomp $smtp_configuration{'username'};
 
     print "SMTP Password: ";
-    $smtp_password = <STDIN>;
-    chomp $smtp_password;
+    $smtp_configuration{'password'} = <STDIN>;
+    chomp $smtp_configuration{'password'};
 
     print "To which email address should the details be sent: ";
     $recipient = <STDIN>;
     chomp $recipient;
+
+    for my $key ( keys %smtp_configuration ) {
+        my $value = $smtp_configuration{$key};
+        if ($value eq '') {
+            print "Not all of the SMTP details were added. \n";
+            exit 0;
+        }
+    }
+
+    if ($recipient eq '') {
+        print "The recipient can not be empty. \n";
+        exit 0;
+    }
+
+    my $transport = Email::Sender::Transport::SMTP->new(
+        host          => $smtp_configuration{'server'},
+        port          => $smtp_configuration{'port'},
+        ssl           => 'ssl',
+        sasl_username => $smtp_configuration{'username'},
+        sasl_password => $smtp_configuration{'password'}
+    );
+
+    my $email = Email::Simple->create(
+        header => [
+            To      => $recipient,
+            From    => $smtp_configuration{'username'},
+            Subject => "Disk usage for " . $directory,
+        ],
+        body   => $message,
+    );
+
+    try {
+        sendmail($email, { transport => $transport });
+    } catch {
+        warn "caught error: $_";
+        exit 0;
+    };
+
+    print "Email Sent Successfully\n";
 }
 
 
-my $transport = Email::Sender::Transport::SMTP->new(
-    host => $smtp_server,
-    port => $smtp_port,
-    ssl => 'ssl',
-    sasl_username => $smtp_username,
-    sasl_password => $smtp_password,
-    helo => 'testingdonetianpetkov'
-);
 
-my $email = Email::Simple->create(
-  header => [
-    To      => 'donetian.petkov@gmail.com',
-    From    => $smtp_username,
-    Subject => 'Hi! Test',
-  ],
-  body => $message,
-);
-
-sendmail($email, { transport => $transport });
-
-
-print "Email Sent Successfully\n";
